ef5dd464eb1e96c0e0fc186ccab232da794ddcf3
Ogion24 <aleksey.inb.s@gmail.com>
2026-04-27 18:24:52 +0300

    second

--- src/components/Profile/ChangePassword.tsx
+++ src/components/Profile/ChangePassword.tsx
@@ -14,7 +14,7 @@ const ChangePassword = ({close}: Props) => {
 
     const handleClickSave = () => {
         if (newPassword === confirmPassword) {
-            dispatch(changePassword(newPassword));
+            dispatch(changePassword({newPassword, oldPassword}));
             close();
         } else {
             alert('New password and confirm password do not match');
