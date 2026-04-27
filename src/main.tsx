3ff6571a44abc9cf624d4830ad34d9848f3a0f81
Ogion24 <aleksey.inb.s@gmail.com>
2026-04-27 12:12:18 +0300

    second

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import {store} from "./app/store.ts"
 import {BrowserRouter} from "react-router";
+import {Provider} from "react-redux";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
-    <App />
-          </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+
+</StrictMode>
 )
